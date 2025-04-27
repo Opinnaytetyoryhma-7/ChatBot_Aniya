5ba5b3d59986e6e072e7635aa880678f5914d246
Alisa <alisa.kulonpaa@gmail.com>
2025-04-27 21:25:08 +0300

    Nyt voi suositella tuotetta

---
+++ backend/main.py
@@ -0,0 +1,180 @@
+from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel, EmailStr
+from chat import get_response, intents
+from backend.database import (
+    save_unknown_message,
+    create_ticket,
+    create_user,
+    get_user_by_email,
+    get_products,
+)
+from fastapi.middleware.cors import CORSMiddleware
+import random
+from backend.auth import (
+    hash_password,
+    verify_password,
+    create_access_token,
+    get_current_user,
+)
+
+app = FastAPI()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# cors
+origins = [
+    "http://localhost:3000",  # React-frontend, muuta tarvittaessa
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # sallii vain määritellyt osoitteet
+    allow_credentials=True,  # esim. JWT-tokenit
+    allow_methods=["*"],  # sallii kaikki HTTP-metodit
+    allow_headers=["*"],  # sallii kaikki headerit
+)
+
+
+# Määritellään ChatInput-malli, joka määrittelee POST-pyynnössä vastaanotetun datan rakenteen
+class ChatInput(BaseModel):
+    message: str  # käyttäjän lähettämä viesti
+    conversation_state: str | None = None  # keskustelun tila, oletuksena None
+
+
+class TicketInput(BaseModel):
+    issue_description: str  # ongelman kuvaus
+    email: EmailStr  # käytetään pydanticin EmailStr-tyyppiä sähköpostiosoitteen kelpoisuuden tarkistamiseen
+
+
+class RegisterInput(BaseModel):
+    email: EmailStr
+    password: str
+    fname: str
+    lname: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+
+class UserOut(BaseModel):  # vältetäään palauttamasta salasanaa frontendille
+    id: str
+    email: EmailStr
+
+
+# Luodaan post-pyyntö, joka vastaanottaa käyttäjän viestin
+@app.post("/chat")
+async def chat(input: ChatInput):
+    tag, prob = get_response(input.message)
+
+    if prob > 0.75:
+        for intent in intents["intents"]:
+            if tag == intent["tag"]:
+                response = random.choice(intent["responses"])
+
+                return {"response": response}
+
+    # Jos botti ei tunnista viestiä, tallennetaan se Supabasen Message-tauluun
+    save_unknown_message(input.message)
+
+    state = input.conversation_state  # kertoo missä kohtaa keskustelu on menossa
+    msg = input.message.strip().lower()
+
+    match state:
+        case None:
+            return {
+                "response": "Pahoittelut, nyt en ymmärtänyt. Haluatko jättää tukipyynnön?",
+                "conversation_state": "ask_ticket",
+            }
+
+        case "ask_ticket":
+            if msg in ["kyllä", "joo", "haluan", "ok", "juu"]:
+                return {
+                    "response": "Voisitko kuvailla ongelmasi ja jättää yhteystietosi, kiitos!",
+                    "conversation_state": "wait_description",
+                }
+            else:
+                return {
+                    "response": "Selvä juttu! Hyvää päivänjatkoa!",
+                    "conversation_state": "end",
+                }
+
+        case "wait_description":
+            return {
+                "response": "Kiitos! Lisää vielä sähköpostiosoitteesi, niin voimme ottaa sinuun yhteyttä",
+                "conversation_state": "wait_email",
+                "issue_description": input.message,  # tallennetaan esim. React useStateen, lähetetään /ticket-pyynnössä
+            }
+
+        case _:
+            return {
+                "response": "Jokin meni pieleen. Yritetäänpä uudelleen.",
+                "conversation_state": None,
+            }
+
+
+# Kun sekä ongelman kuvaus että sähköpostiosoite on saatu, muodostetaan uusi post-pyyntö
+@app.post("/ticket")
+async def ticket(input: TicketInput):
+    create_ticket(input.issue_description, input.email)
+
+    return {
+        "response": "Kiitos! Olemme tallentaneet tietosi. Palaamme asiaan heti kun pystymme!"
+    }
+
+
+@app.post("/register", response_model=UserOut)
+async def register_user(user: RegisterInput):
+    existing_user = get_user_by_email(user.email)
+    if existing_user.data:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Käyttäjä on jo olemassa"
+        )
+
+    hashed_pw = hash_password(user.password)
+    new_user = (
+        create_user(
+            fname=user.fname,
+            lname=user.lname,
+            email=user.email,
+            password=hashed_pw,
+        )
+        .execute()
+        .data[0]
+    )
+
+    return {"id": new_user["user_id"], "email": new_user["email"]}
+
+
+@app.post("/token", response_model=Token)
+async def login(form_data: OAuth2PasswordRequestForm = Depends()) -> Token:
+    res = get_user_by_email(form_data.username)
+    if not res.data:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Käyttäjätunnus tai salasana virheellinen",
+        )
+    user = res.data[0]
+    if not verify_password(form_data.password, user["password"]):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Käyttäjätunnus tai salasana virheellinen",
+        )
+
+    token = create_access_token({"sub": str(user["user_id"])})
+
+    return Token(
+        access_token=token, token_type="bearer"
+    )  # frontendille lähetettävä token
+
+
+@app.get("/users/me", response_model=UserOut)  # palauttaa kirjautuneen käyttäjän tiedot
+async def read_users_me(current_user=Depends(get_current_user)):
+    return {"id": current_user["user_id"], "email": current_user["email"]}
+
+
+@app.get("/products")
+async def get_products_list():
+    products = get_products()
+    return products.data
---
+++ chat.py
@@ -0,0 +1,183 @@
+#Random: selects random responses for variety
+#Json: loads the chatbot’s predefined questions and responses
+#Torch: loads and runs the trained Neural Network model
+#NeuralNet: imports the chatbot’s model from model.py
+#bag_of_words, tokenize: preprocess user input
+import random
+import json
+import torch
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
+from datetime import datetime
+
+#Defined the chatbot's name
+#Enables timestamp
+#Uses GPU if available
+bot_name = 'Aniya'
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+#Loads intents.json, which contains chatbot responses
+with open('intents.json', 'r', encoding='utf-8') as f:
+    intents = json.load(f)
+
+#Loads products.json for matching product with keyword 
+def recommend_product(user_input):
+    with open('products.json', 'r', encoding='utf-8') as f:
+        products_data = json.load(f)
+        products = products_data["products"]
+
+    recommended = products
+
+    # Priority filters
+    if "muisti" in user_input.lower():
+        recommended = [p for p in recommended if "RAM" in p["Category"]]
+
+    if "tietokone" in user_input.lower() or "kone" in user_input.lower():
+        recommended = [p for p in recommended if "Computer" in p["Category"]]
+
+    if "läppäri" in user_input.lower() or "kannettava" in user_input.lower():
+        recommended = [p for p in recommended if "Laptop" in p["Category"]]
+
+    if "näppäimistö" in user_input.lower():
+        recommended = [p for p in recommended if "Keyboard" in p["Category"]]
+
+    if "hiir" in user_input.lower():
+        recommended = [p for p in recommended if "Mouse" in p["Category"]]
+
+    if "budjetti" in user_input.lower() or "edullinen" in user_input.lower() or "halpa" in user_input.lower():
+        recommended = sorted(recommended, key=lambda x: x["price"])
+
+    # Return top 3 best matches
+    return recommended[:3]
+
+
+def load_model():
+    #Loads the trained model from data.pth
+    FILE = "data.pth"
+    data = torch.load(FILE, map_location=device)
+
+    #Recreates the Neural Network model using the stored parameters
+    #Loads trained weights, model_state, into the model
+    #Sets the model to evaluation mode, eval(), disabling training behaviors
+    #Extracts metadata from the saved data.pth
+    #These values are used to rebuild the model
+    model = NeuralNet(data["input_size"], data["hidden_size"], data["output_size"]).to(device)
+    model.load_state_dict(data["model_state"])
+    model.eval()
+
+    return model, data["all_words"], data["tags"]
+
+model, all_words, tags = load_model()
+
+def get_response(raw_sentence):
+
+    #Tokenizes the input
+    #Converts it into a bag-of-words vector
+    #Reshapes it into a format suitable for the model
+    #Converts it into a PyTorch tensor for processing
+    #Save original input
+    sentence = tokenize(raw_sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    #Passes input through the model to get predictions
+    #Finds the intent with the highest probability
+    #Retrieves the corresponding tag from the tags list
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+    tag = tags[predicted.item()]
+
+    #Applies softmax to convert model output into probabilities
+    #Retrieves the probability of the predicted intent
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+
+    return tag, prob.item()
+
+# Track user messages
+# Save to log only if it's "unknown"
+def log_unknown_input(sentence):
+    with open("chat_logs.txt", "a", encoding="utf-8") as log_file:
+        log_file.write(f"[{timestamp}] USER: {sentence.strip()} | INTENT: unknown\n")
+
+#creating a ticket
+def handle_ticket(sentence):
+    print(f"{bot_name}: Voisitko kuvailla ongelmasi ja jättää yhteystietosi, kiitos!")
+    ticket = input("Sinä: ")
+
+    with open("tickets.txt", "a", encoding="utf-8") as ticket_file:
+        ticket_file.write(f"[{timestamp}]\nCONTACT REQUEST:\nMessage: {sentence.strip()}\nUser Info: {ticket.strip()}\n---\n")
+
+    print(f"{bot_name}: Kiitos! Olemme tallentaneet tietosi. Palaamme asiaan heti kun pystymme!")
+
+def chat_loop():
+
+    #Starts an infinite loop that waits for user input
+    #Exits when the user types quit
+    print("Hei! Miten voin olla avuksi? :) Jos haluat lopettaa keskustelun, kirjoita stop.")
+
+    waiting_for_recom_desc = False
+
+    while True:
+        raw_sentence = input('Sinä: ')
+        if raw_sentence == "stop":
+            break
+
+        # Special case: If we are waiting for the description after asking "millaista laitetta"
+        if waiting_for_recom_desc:
+            recoms = recommend_product(raw_sentence)
+            if recoms:
+                print(f"{bot_name}: Tässä muutama vaihtoehto sinulle:")
+                for product in recoms:
+                    print(f"- {product['name']} ({product['price']}€)")
+            else:
+                print(f"{bot_name}: Valitettavasti en löytänyt sopivaa tuotetta kuvauksen perusteella.")
+            waiting_for_recom_desc = False  # reset
+            continue
+
+        tag, prob = get_response(raw_sentence)
+
+        #If the probability is above 75%, the bot picks a response from intents.json
+        #Otherwise, it responds with Pahoittelut, nyt en ymmärtänyt. Haluatko jättää yhteydenottopyynnön?
+        if prob > 0.75:
+            for intent in intents["intents"]:
+                if tag == intent["tag"]:
+                    if tag == "recommend_product":
+                        recoms = recommend_product(raw_sentence)
+                        print(f"{bot_name}: Tässä parhaat ehdotukset:")
+                        for product in recoms:
+                            print(f"- {product['name']} ({product['price']}€)")
+                        else:
+                                print(f"{bot_name}: En löytänyt sopivaa tuotetta annetuilla hakuehdoilla.")
+                
+                    elif tag == "recommend":
+                        print(f"{bot_name}: {random.choice(intent['responses'])}")
+                        waiting_for_recom_desc = True
+    
+                    else:
+                        print(f"{bot_name}: {random.choice(intent['responses'])}")
+                    
+                    if tag == "goodbye":
+                        return
+                    elif tag == "problem":
+                        if tag == "ticket_asking":
+                            handle_ticket(raw_sentence)
+                            return
+                        if tag == "goodbye":
+                            return
+                    break
+        else:
+            print(f"{bot_name}: Pahoittelut, nyt en ymmärtänyt. Haluatko jättää yhteydenottopyynnön?")
+            log_unknown_input(raw_sentence)
+
+            follow = input("Sinä: ").lower()
+            if follow in ["kyllä", "joo", "haluan", "ok", "juu"]:
+                handle_ticket(raw_sentence)
+            else:
+                print(f"{bot_name}: Selvä juttu! Hyvää päivänjatkoa!")
+            return
+        
+if __name__ == "__main__":
+    chat_loop()
